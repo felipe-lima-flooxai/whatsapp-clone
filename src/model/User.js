174229c6018126be37ab05d4f19d9ac4c4dbf216
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-01 13:28:59 -0300

    feat: adds addContact

--- src/model/User.js
+++ src/model/User.js
@@ -43,7 +43,36 @@ export class User extends Model{
         return User.getRef().doc(email);
     }
 
+    static getContactsRef(id){
+        return User.getRef().doc(id).collection("contacts");
+    }
+
     save(){
         return User.findByEmail(this.email).set(this.toJSON());
+    }
+
+    addContact(contact) {
+        return User.getContactsRef(this.email).doc(btoa(contact.email)).set(contact.toJSON());
+    }
+
+    getContacts(){
+        return new Promise((s, f)=>{
+            User.getContactsRef(this.email).onSnapshot(docs =>{
+
+                let contacts = [];
+
+                docs.forEach(doc => {
+
+                    let data = doc.data();
+                    data.id = doc.id;
+                    contacts.push(data);
+
+                });
+
+                this.trigger("contactschange", docs);
+                s(contacts);
+
+            });
+        });
     }
 }
