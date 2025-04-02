b1299889b5ff08af57bfce9029fc578c90cd6139
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-02 09:52:49 -0300

    feat: chat in db now

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,9 +2,9 @@ import {Format} from "./../util/Format";
 import {CameraController} from "./CameraController";
 import {MicrophoneController} from "./MicrophoneController";
 import {DocumentPreviewController} from "./DocumentPreviewController";
-import {Firebase} from "./../util/Firebase"
-import {User} from "./../model/User"
-
+import {Firebase} from "./../util/Firebase";
+import {User} from "./../model/User";
+import {Chat} from "./../model/Chat"
 
 export class WhatsAppController {
     constructor(){
@@ -278,10 +278,20 @@ export class WhatsAppController {
             contact.on("datachange", data=>{
                 if(data.name){
 
-                    this._user.addContact(contact).then(()=>{
+                    Chat.createIfNotExists().then(chat =>{
 
-                        this.el.btnClosePanelAddContact.click();
-                        console.info("Contato foi adicionado!");
+                        contact.chatId = chat.id;
+
+                        this._user.chatId = chat.id;
+
+                        contact.addContact(this._user);
+
+                        this._user.addContact(contact).then(()=>{
+
+                            this.el.btnClosePanelAddContact.click();
+                            console.info("Contato foi adicionado!");
+                    });
+
                     });
 
                 } else {
